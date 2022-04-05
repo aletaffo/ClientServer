0d50cd31cfb610ae38bbca15f42ea46bdf074109
Studente <Studente@LAPTOP-BDMS826T>
2022-04-05 09:37:32 +0200

    inizio

---
+++ src/main/java/com/mycompany/clientserver/Client.java
@@ -0,0 +1,11 @@
+package com.mycompany.clientserver;
+
+/**
+ *
+ * @author Studente
+ */
+public class Client {
+    public static void main(String[] args){
+         
+     } 
+}
---
+++ src/main/java/com/mycompany/clientserver/Server.java
@@ -0,0 +1,11 @@
+package com.mycompany.clientserver;
+
+/**
+ *
+ * @author Studente
+ */
+public class Server {
+     public static void main(String[] args){
+         
+     } 
+}
