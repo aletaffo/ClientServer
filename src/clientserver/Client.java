ad4ce9ba26741d8c14ecf855722627db9031113e
aletaffo <alessandrotaffini@gmail.com>
2022-04-07 10:16:16 +0200

    Start with my pc

--- src/clientserver/Client.java
+++ src/clientserver/Client.java
@@ -1,6 +1,9 @@
 package clientserver;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -9,6 +12,11 @@ import java.net.Socket;
  */
 public class Client {
         public static void main (String[] args){
-            
+            try {
+                Socket socket = new Socket("127.0.0.1",2000);
+                //Apro un socket specificando l'indirizzo IP e il numero di porta del server.
+            } catch (IOException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
     }
 }
